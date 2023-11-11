ddbac5a655b9b8069eacc76ca63f78eb5516286c
Ipikk <irvityara456@gmail.com>
2023-11-11 08:46:58 +0700

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const route = express.Router()
 const todoRoutes = require("./todo-route")
+const userRoutes = require("./user-route")
 
 route.get("/", (req, res) => {
 })
 
 route.use("/todos", todoRoutes)
-// route.use("/users", userRoutes)
+route.use("/users", userRoutes)
 
 module.exports = route
